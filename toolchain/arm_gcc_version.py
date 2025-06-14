9006ef6f8780774a27cf76ccc45d672fb1c89e23
Jalon Wong <jalonwong@gmail.com>
2025-06-13 20:35:44 -0500

    Format scripts

--- toolchain/arm_gcc_version.py
+++ toolchain/arm_gcc_version.py
@@ -1,9 +1,9 @@
-import re
 import os
-import sys
+import re
 import subprocess
+import sys
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     if len(sys.argv) >= 2:
         arm_path = sys.argv[1]
         if not os.path.exists(arm_path):
@@ -11,12 +11,12 @@ if __name__ == '__main__':
     else:
         exit(1)
 
-    arm_bin = os.path.join(arm_path, 'bin', 'arm-none-eabi-gcc')
-    ret = subprocess.run([arm_bin, '--version'], capture_output=True, text=True)
+    arm_bin = os.path.join(arm_path, "bin", "arm-none-eabi-gcc")
+    ret = subprocess.run([arm_bin, "--version"], capture_output=True, text=True)
     if ret.returncode != 0:
         exit(ret.returncode)
 
-    obj = re.search('\) ([\d\.]+)', ret.stdout)
+    obj = re.search(r"\) ([\d\.]+)", ret.stdout)
     if obj:
         print(obj.group(1))
         exit(0)
--- toolchain/redirect.py
+++ toolchain/redirect.py
@@ -1,12 +1,12 @@
-import sys
 import subprocess
+import sys
 
 cmd = sys.argv[1:-1]
 output = sys.argv[-1]
 
 ret = subprocess.run(cmd, text=True, capture_output=True)
 if ret.returncode == 0:
-    with open(output, 'w') as f:
+    with open(output, "w") as f:
         f.write(ret.stdout)
 
 exit(ret.returncode)
